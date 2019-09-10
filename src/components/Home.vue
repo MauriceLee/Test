da65ee91bcc7ead985695fc9a5da36128941e100
MauriceLee <ee.maurice.lee@gmail.com>
2019-09-10 19:55:28 +0800

    Initial commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,54 +3,76 @@
     <div class="jumbotron jumbotron-fluid">
       <h1>SnowBoarding For Life</h1>
       <p>
-          <br />
-          I am a engineer from taiwan.
-          <br />
-          Named Maurice.
-          <br />
-          Someone likes snowboarding very much.
-          <br />
-          Hope one day I can go to Niseko in Japan Hokkaido.
-          <br />
-          So I build this website and a Todolist program to help me to get my dream.
+        <br />I am a engineer from taiwan.
+        <br />Named Maurice.
+        <br />Someone likes snowboarding very much.
+        <br />Hope one day I can go to Niseko in Japan Hokkaido.
+        <br />So I build this website and a Todolist program to help me to get my dream.
       </p>
     </div>
 
     <div class="card-group">
       <div class="card">
-        <img src="../assets/img1.jpg" class="card-img-top" alt="im1">
+        <img src="../assets/img1.jpg" class="card-img-top" alt="im1" />
         <div class="card-body">
           <h5 class="card-title">About Niseko United</h5>
           <p class="card-text">
             Niseko United is comprised of four resorts on the one mountain, Niseko Annupuri (1,308m).
-            <br /><br />
-            100km south of Sapporo, Niseko Annupuri is a part of the Niseko-Shakotan-Otaru Kaigan Quasi-National Park and is the most eastern park of the Niseko Volcanic Group.
-            <br /><br />
-            Seasonal winds from the Eurasian continent pick up moisture over the warm currents of the Sea of Japan to from snow clouds and in turn some of the driest, lightest powder in the world.
-            <br /><br />
-            With its rich variety of terrain and beautiful winter woods, Niseko offers an unforgettable experience for all levels of skier/snowborder.
+            <br />
+            <br />100km south of Sapporo, Niseko Annupuri is a part of the Niseko-Shakotan-Otaru Kaigan Quasi-National Park and is the most eastern park of the Niseko Volcanic Group.
+            <br />
+            <br />Seasonal winds from the Eurasian continent pick up moisture over the warm currents of the Sea of Japan to from snow clouds and in turn some of the driest, lightest powder in the world.
+            <br />
+            <br />With its rich variety of terrain and beautiful winter woods, Niseko offers an unforgettable experience for all levels of skier/snowborder.
           </p>
         </div>
       </div>
 
       <div class="card text-center">
-        <img src="../assets/img2.jpg" class="card-img-top" alt="img2">
+        <img src="../assets/img2.jpg" class="card-img-top" alt="img2" />
         <div class="card-body">
           <h5 class="card-title">Current Weather</h5>
-          <p class="card-text">
-
-          </p>
-          <h5 class="card-title"><br /><br />Website Link</h5>
+          <div class="table-responsive">
+          <table class="table">
+            <tbody>
+              <tr>
+                <th scope="row">Country</th>
+                <td>{{ weatherItems[0] }}</td>
+              </tr>
+              <tr>
+                <th scope="row">city</th>
+                <td>{{ weatherItems[1] }}</td>
+              </tr>
+              <tr>
+                <th scope="row">weather</th>
+                <td>{{ weatherItems[2] }}</td>
+              </tr>
+              <tr>
+                <th scope="row">Temperature</th>
+                <td>{{ weatherItems[3] }}</td>
+              </tr>
+            </tbody>
+          </table>
+          </div>
+          <h5 class="card-title">
+            <br />Website Link
+          </h5>
           <a class="btn btn-primary btn-lg" href="http://www.niseko.ne.jp" role="button">Push Me</a>
         </div>
       </div>
 
       <div class="card">
-        <img src="../assets/img3.jpg" class="card-img-top" alt="img3">
+        <img src="../assets/img3.jpg" class="card-img-top" alt="img3" />
         <div class="card-body">
           <h5 class="card-title">Niseko United PV 2018</h5>
           <div class="video-container">
-            <iframe title="NISEKO UNITED PV 2018" src="https://www.youtube.com/embed/G_qFhknHEiY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe
+              title="NISEKO UNITED PV 2018"
+              src="https://www.youtube.com/embed/G_qFhknHEiY"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
           </div>
         </div>
       </div>
@@ -60,11 +82,26 @@
 
 <script>
 export default {
-  name: 'Home',
   data () {
     return {
-      msg: 'Home'
+      weatherItems: []
     }
+  },
+  created () {
+    const api =
+      'https://api.openweathermap.org/data/2.5/forecast?q=Niseko,JP&APPID=c674981b3e07e38b9b49ac5b4b480167&units=metric'
+    const self = this
+    this.$http.get(api).then(response => {
+      let apiData = response.data
+      // console.log(apiData)
+      self.weatherItems.push(
+        apiData.city.country,
+        apiData.city.name,
+        apiData.list[0].weather[0].main,
+        apiData.list[0].main.temp
+      )
+      // console.log(self.weatherItems)
+    })
   }
 }
 </script>
@@ -73,6 +110,7 @@ export default {
 <style scoped>
 .jumbotron {
   padding: 0;
+  margin-bottom: 1rem;
 }
 h1 {
   font-size: 28px;
@@ -81,18 +119,27 @@ h1 {
   text-align: center;
   font-size: 27px;
 }
-.video-container {
-position: relative;
-padding-bottom: 56.25%;
-padding-top: 30px;
-height: 0;
-overflow: hidden;
+.table {
+  color: #000000;
+  font-family: monospace;
+  font-size: 17px;
+  text-align: center;
+  margin: auto;
 }
-.video-container iframe, .video-container object, .video-container embed {
-position: absolute;
-top: 0;
-left: 0;
-width: 100%;
-height: 100%;
+.video-container {
+  position: relative;
+  padding-bottom: 56.25%;
+  padding-top: 30px;
+  height: 0;
+  overflow: hidden;
+}
+.video-container iframe,
+.video-container object,
+.video-container embed {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -1,34 +1,185 @@
 <template>
-  <div class="Todolist">
-    <h1>{{ msg }}</h1>
+  <div>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">待辦事項</span>
+      </div>
+      <input
+        type="text"
+        class="form-control"
+        placeholder="準備要做的任務"
+        v-model="newTodo"
+        @keyup.enter="addTodo"
+      />
+      <div class="input-group-append">
+        <button class="btn btn-primary" type="button" @click="addTodo">新增</button>
+      </div>
+    </div>
+    <div class="card text-center">
+      <div class="card-header">
+        <ul class="nav nav-tabs card-header-tabs">
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              :class="{'active': visibility == 'all'}"
+              @click="visibility = 'all'"
+              href="#"
+            >全部</a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              :class="{'active': visibility == 'active'}"
+              @click="visibility = 'active'"
+              href="#"
+            >進行中</a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              :class="{'active': visibility == 'completed'}"
+              @click="visibility = 'completed'"
+              href="#"
+            >已完成</a>
+          </li>
+        </ul>
+      </div>
+      <ul class="list-group list-group-flush text-left">
+        <li
+          class="list-group-item"
+          v-for="item in filterdTodos"
+          :key="item.id"
+          @dblclick="editTodo(item)"
+        >
+          <div class="d-flex" v-if="item.id !== cacheTodo.id">
+            <div class="form-check">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                v-model="item.completed"
+                :id="item.id"
+              />
+              <label
+                class="form-check-label"
+                :class="{'completed': item.completed}"
+                :for="item.id"
+              >{{ item.title }}</label>
+            </div>
+            <button
+              type="button"
+              class="close ml-auto"
+              @click="removeTodo(item)"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <input
+            type="text"
+            class="form-control"
+            v-model="cacheTitle"
+            @keyup.esc="cancelEdit"
+            @keyup.enter="doneEdit(item)"
+            v-if="item.id === cacheTodo.id"
+          />
+        </li>
+      </ul>
+      <div class="card-footer d-flex justify-content-between">
+        <span>還有 {{ count }} 筆任務未完成</span>
+        <a href="#" @click="removeAll">清除所有任務</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Todolist',
   data () {
     return {
-      msg: 'Todolist'
+      newTodo: '',
+      todos: JSON.parse(localStorage.getItem('TodosList')) || [],
+      visibility: 'all',
+      cacheTodo: {},
+      cacheTitle: '',
+      count: 0
     }
+  },
+  methods: {
+    addTodo: function () {
+      let value = this.newTodo.trim()
+      let timestamp = Math.floor(Date.now())
+      if (!value) {
+        return
+      }
+      this.todos.push({
+        id: timestamp,
+        title: value,
+        completed: false
+      })
+      this.newTodo = ''
+    },
+    removeTodo: function (todo) {
+      let self = this
+      let newIndex = self.todos.findIndex(function (item, key) {
+        return todo.id === item.id
+      })
+      this.todos.splice(newIndex, 1)
+    },
+    editTodo: function (item) {
+      this.cacheTodo = item
+      this.cacheTitle = item.title
+    },
+    cancelEdit: function () {
+      this.cacheTodo = {}
+    },
+    doneEdit: function (item) {
+      item.title = this.cacheTitle
+      this.cacheTitle = {}
+      this.cacheTodo = {}
+    },
+    removeAll: function () {
+      this.todos = []
+    }
+  },
+  computed: {
+    filterdTodos: function () {
+      if (this.visibility === 'all') {
+        return this.todos
+      } else if (this.visibility === 'active') {
+        let newTodos = []
+        this.todos.forEach(function (item) {
+          if (!item.completed) {
+            newTodos.push(item)
+          }
+        })
+        return newTodos
+      } else if (this.visibility === 'completed') {
+        let newTodos = []
+        this.todos.forEach(function (item) {
+          if (item.completed) {
+            newTodos.push(item)
+          }
+        })
+        return newTodos
+      }
+    }
+  },
+  updated () {
+    localStorage.setItem('TodosList', JSON.stringify(this.todos))
+    let c = 0
+    for (let i = 0; i < this.todos.length; i++) {
+      if (!this.todos[i].completed) {
+        c++
+      }
+    }
+    this.count = c
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.completed {
+  text-decoration: line-through;
 }
 </style>
